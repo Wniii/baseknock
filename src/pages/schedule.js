6388c05361a4066b44380aa59896f08252bb0f25
tsang10 <vivit50923@gmail.com>
2024-03-07 09:56:57 +0800

    賽程表

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,58 +1,60 @@
+import React from 'react';
 import Head from 'next/head';
-import { Box, Container, Stack, Typography, Unstable_Grid2 as Grid, Divider } from '@mui/material';
+import { Box, Container, Stack, Typography, Grid, Divider } from '@mui/material';
 import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import { Calendar } from 'src/sections/schedule/schedule-calendar';
+import Calendar from 'rsuite/Calendar';
+import 'rsuite/Calendar/styles/index.css';
 
+const SchedulePage = () => {
+    const newLocale = {
+        sunday: 'SUN',
+        monday: 'MON',
+        tuesday: 'TUE',
+        wednesday: 'WED',
+        thursday: 'THU',
+        friday: 'FRI',
+        saturday: 'SAT',
 
+    };
 
-const Page = () => (
-    <>
-        <Head>
-            <title>
-                賽程表
-            </title>
-        </Head>
-        <Box
-            component="main"
-            sx={{
-                flexGrow: 1,
-                py: 0
-            }}
-        >
-            <Container maxWidth="lg">
-                <Stack spacing={3}>
-                    <div>
-                        <Typography variant="h4">
-                            賽程表
-                        </Typography>
-                    </div>
-                    <Divider />
-                    <div>
-                        <Grid
-                            container
-                            spacing={3}
-                        >
-
-                            <Grid
-                                xs={12}
-                                md={12}
-                                lg={12}
-                            >
-                                <Calendar />
+    return (
+        <>
+            <Head>
+                <title>賽程表</title>
+            </Head>
+            <Box
+                component="main"
+                sx={{
+                    flexGrow: 1,
+                    py: 0
+                }}
+            >
+                <Container maxWidth="lg">
+                    <Stack spacing={3}>
+                        <div>
+                            <Typography variant="h4">賽程表</Typography>
+                        </div>
+                        <Divider />
+                        <div>
+                            <Grid container spacing={3}>
+                                <Grid xs={12} md={12} lg={12}>
+                                    <Calendar
+                                        locale={newLocale}
+                                    />
+                                </Grid>
                             </Grid>
-                        </Grid>
-                    </div>
-                </Stack>
-            </Container>
+                        </div>
+                    </Stack>
+                </Container>
+            </Box>
+        </>
+    );
+};
 
-        </Box>
-    </>
-);
-
-Page.getLayout = (page) => (
+SchedulePage.getLayout = (page) => (
     <DashboardLayout>
         {page}
     </DashboardLayout>
 );
 
-export default Page;
+export default SchedulePage;
