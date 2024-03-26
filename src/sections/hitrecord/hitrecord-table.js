d112b6406619bf56cff43fa9b086b44714960438
Veronica0526 <47806022+Veronica0526@users.noreply.github.com>
2024-03-26 11:24:10 +0800

    hit

--- src/sections/hitrecord/hitrecord-table.js
+++ src/sections/hitrecord/hitrecord-table.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { format } from 'date-fns';
 import {
@@ -20,10 +20,12 @@ import {
   DialogActions,
 } from '@mui/material';
 import { Scrollbar } from 'src/components/scrollbar';
-import { getInitials } from 'src/utils/get-initials';
 import SearchIcon from '@mui/icons-material/Search';
 
-export const CustomersTable = (props) => {
+import { firestore } from 'src/pages/firebase'; // 导入 firestore
+import { collection, getDocs } from "firebase/firestore"; // 导入获取文档的方法
+
+export const HitrecordTable = (props) => {
   const {
     count = 0,
     items = [],
@@ -31,17 +33,38 @@ export const CustomersTable = (props) => {
   } = props;
 
   const [open, setOpen] = useState(false);
-  const [selectedCustomer, setSelectedCustomer] = useState(null);
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [playersData, setPlayersData] = useState([]); // 用于存储从数据库中获取的球员数据
 
-  const handleOpen = (customer) => {
-    setSelectedCustomer(customer);
+
+  const handleOpen = (player) => {
+    setSelectedPlayer(player);
     setOpen(true);
   };
 
-  const handleClose = () => {
+  const handleClose = () => {0
     setOpen(false);
   };
 
+  
+
+  useEffect(() => {
+    // 在组件加载时从数据库中获取球员数据
+    const fetchPlayersData = async () => {
+      const playersCollection = collection(firestore, "player");
+      const querySnapshot = await getDocs(playersCollection);
+      const data = [];
+      querySnapshot.forEach((doc) => {
+        data.push(doc.data());
+      });
+      setPlayersData(data);
+    };
+    fetchPlayersData();
+  }, []); // 空依赖项，表示仅在组件加载时运行一次
+
+  
+
+  
   return (
     <Card>
       <Scrollbar>
@@ -126,63 +149,56 @@ export const CustomersTable = (props) => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {items.map((customer) => {
-                const isSelected = selected.includes(customer.id);
-                const createdAt = format(customer.createdAt, 'dd/MM/yyyy');
+                {items.map((player, index) => {
+                  const isSelected = selected.includes(player.id);
+                  // const createdAt = format(player.createdAt, 'dd/MM/yyyy');
 
                 return (
-                  <TableRow hover key={customer.id} selected={isSelected}>
+                  <TableRow hover key={player.id} selected={isSelected}>
                     <TableCell>
-                      <Button size="small" startIcon={<SearchIcon />} onClick={() => handleOpen(customer)}>
+                      <Button size="small" startIcon={<SearchIcon />} onClick={() => handleOpen(player)}>
                         查看
                       </Button>
                     </TableCell>
                     <TableCell>
-                    <Stack
-                        alignItems="center"
-                        direction="row"
-                        spacing={2}
-                      >
-                        <Avatar src={customer.avatar}>
-                          {getInitials(customer.name)}
-                        </Avatar>
                         <Typography variant="subtitle2">
-                          {customer.name}
+                          {index + 1} {/* 替代 player.name */}
                         </Typography>
-                      </Stack>
                     </TableCell>
                     <TableCell>
-                      {customer.email}
+                      {player.p_name}
                     </TableCell>
                     <TableCell>
-                      {customer.address.city}, {customer.address.state}, {customer.address.country}
+                      {/* {player.address.city}, {player.address.state}, {player.address.country} */}
                     </TableCell>
                     <TableCell>
-                      {customer.phone}
+                      {player.phone}
                     </TableCell>
                     <TableCell>
-                      {createdAt}
+                      {/* {createdAt} */}
                     </TableCell>
 
                   </TableRow>
                 );
               })}
+            
             </TableBody>
           </Table>
         </Box>
       </Scrollbar>
-      <CustomerDialog open={open} onClose={handleClose} customer={selectedCustomer} />
+      <PlayerDialog open={open} onClose={handleClose} player={selectedPlayer} />
     </Card>
   );
 };
+  
 
-CustomersTable.propTypes = {
+HitrecordTable.propTypes = {
   count: PropTypes.number,
   items: PropTypes.array,
   selected: PropTypes.array
 };
 
-const CustomerDialog = ({ open, onClose, customer }) => {
+const PlayerDialog = ({ open, onClose, player }) => {
   const handleClose = () => {
     onClose();
   };
@@ -191,9 +207,9 @@ const CustomerDialog = ({ open, onClose, customer }) => {
     <Dialog open={open} onClose={handleClose}>
      
       <DialogContent>
-        {customer && (
+        {player && (
           <>
-           <DialogTitle>{customer.name}</DialogTitle>
+           <DialogTitle>{player.p_name}</DialogTitle>
             <DialogContentText>
             <img
         src='https://media.istockphoto.com/id/1269757192/zh/%E5%90%91%E9%87%8F/%E6%A3%92%E7%90%83%E5%A0%B4%E5%9C%96%E7%A4%BA%E6%A3%92%E7%90%83%E5%A0%B4%E5%90%91%E9%87%8F%E8%A8%AD%E8%A8%88%E7%9A%84%E5%B9%B3%E9%9D%A2%E5%9C%96%E8%A7%A3%E9%A0%82%E8%A6%96%E5%9C%96-web.jpg?s=612x612&w=0&k=20&c=Zt85Kr6EksFKBmYQmgs138zfLRp3eoIzKeQLS2mirLU='
@@ -204,7 +220,7 @@ const CustomerDialog = ({ open, onClose, customer }) => {
         width={'250px'}
       />
             </DialogContentText>
-            {/* 其他客戶詳細資訊 */}
+            {/* 其他球員詳細資訊 */}
           </>
         )}
       </DialogContent>
@@ -215,10 +231,10 @@ const CustomerDialog = ({ open, onClose, customer }) => {
   );
 };
 
-CustomerDialog.propTypes = {
+PlayerDialog.propTypes = {
   open: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
-  customer: PropTypes.object
+  player: PropTypes.object
 };
 
-export default CustomersTable;
+export default HitrecordTable;
