a246d3615514a4f33814abc8df9781aa10c996b1
Veronica0526 <47806022+Veronica0526@users.noreply.github.com>
2024-04-09 15:31:00 +0800

    hi

--- src/sections/team/team-profile-details.jsx
+++ src/sections/team/team-profile-details.jsx
@@ -15,7 +15,7 @@ import { addDoc, collection, doc, setDoc } from "firebase/firestore"; // 正确�
 
 
 export default function CreateTeamDocumentButton() {
-  const [teamId, setTeamId] = useState(generateRandomBlistId()); // 球队ID
+  const [teamId, setTeamId] = useState(""); // 球队ID
   const [name, setName] = useState(""); // 球队名称
   const [codeName, setCodeName] = useState(""); // 代號
   const [introduction, setIntro] = useState(""); // 簡介
@@ -23,7 +23,6 @@ export default function CreateTeamDocumentButton() {
 
   const handleCreateTeamDocument = async (e) => {
       e.preventDefault();
-
 
       try {
           // 创建一个名为 "team" 的集合，并在其中创建一个球队文档
