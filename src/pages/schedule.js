e86374bad3141007eaeeef31acabb2f9c65f5bdf
tsang10 <vivit50923@gmail.com>
2024-04-27 01:07:08 +0800

    好了的賽程表權限

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -42,111 +42,114 @@ const SchedulePage = () => {
     hometeam: "",
     awayteam: "",
   });
-  const [userTeam, setUserTeam] = useState([]);
+  const userTeam = localStorage.getItem('userTeam') ? localStorage.getItem('userTeam').split(',') : [];
 
-  useEffect(() => {
-    const userTeamString = localStorage.getItem("userTeam");
-    if (userTeamString) {
-      setUserTeam(userTeamString.split(",")); // 将字符串解析为数组
-    }
-  }, []);
+  // useEffect(() => {
+  //   const userTeamString = localStorage.getItem("userTeam");
+  //   if (userTeamString) {
+  //     setUserTeam(userTeamString.split(",")); // 将字符串解析为数组
+  //   }
+  // }, []);
+  
 
 
   const fetchGames = async () => {
     try {
-        const teamCollection = collection(firestore, "team");
-        const teamSnapshot = await getDocs(teamCollection);
+      const teamCollection = collection(firestore, "team");
+      const teamSnapshot = await getDocs(teamCollection);
 
-        // 獲取所有隊伍資料，以便進行 ID 和 名稱查找
-        const teamsData = await getDocs(collection(firestore, "team"));
-        const teamIdMap = new Map();
-        const teamNameMap = new Map();
-        teamsData.forEach(doc => {
-            const data = doc.data();
-            teamIdMap.set(data.codeName, doc.id);
-            teamNameMap.set(data.codeName, data.Name);  // 保存 codeName 到 name 的映射
-        });
+      // 獲取所有隊伍資料，以便進行 ID 和 名稱查找
+      const teamsData = await getDocs(collection(firestore, "team"));
+      const teamIdMap = new Map();
+      const teamNameMap = new Map();
+      teamsData.forEach(doc => {
+        const data = doc.data();
+        teamIdMap.set(data.codeName, doc.id);
+        teamNameMap.set(data.codeName, data.Name);  // 保存 codeName 到 name 的映射
+      });
 
-        const gamesData = [];
-        const addedTimestamps = new Set();
+      const gamesData = [];
+      const addedTimestamps = new Set();
 
-        for (const teamDoc of teamSnapshot.docs) {
-            const teamId = teamDoc.id;
-            const teamData = teamDoc.data();
-            const teamGames = teamData.games;
+      for (const teamDoc of teamSnapshot.docs) {
+        const teamId = teamDoc.id;
+        const teamData = teamDoc.data();
+        const teamGames = teamData.games;
 
-            if (teamGames) {
-                for (const timestamp of Object.keys(teamGames)) {
-                    const game = teamGames[timestamp];
-                    const docRef = doc(firestore, "team", teamId, "games", timestamp);
-                    const docSnap = await getDoc(docRef);
-                    if (docSnap.exists()) {
-                        console.log("Document data:", docSnap.data()); // Debug log
-                        const gameData = docSnap.data();
+        if (teamGames) {
+          for (const timestamp of Object.keys(teamGames)) {
+            const game = teamGames[timestamp];
+            const docRef = doc(firestore, "team", teamId, "games", timestamp);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+              console.log("Document data:", docSnap.data()); // Debug log
+              const gameData = docSnap.data();
 
-                        if (!gameData.hometeam || !gameData.awayteam) {
-                            console.log(gameData.hometeam, gameData.awayteam);
-                            console.error("Game data is missing team information");
-                            continue; // Skip this game if team data is incomplete
-                        }
+              if (!gameData.hometeam || !gameData.awayteam) {
+                console.log(gameData.hometeam, gameData.awayteam);
+                console.error("Game data is missing team information");
+                continue; // Skip this game if team data is incomplete
+              }
 
-                        const hteam = teamIdMap.get(gameData.hometeam);
-                        const ateam = teamIdMap.get(gameData.awayteam);
-                        const homeTeamName = teamNameMap.get(gameData.hometeam);
-                        const awayTeamName = teamNameMap.get(gameData.awayteam);
-                        const title = `${homeTeamName} v.s. ${awayTeamName}`; //Name
-                        //const title = `${gameData.hometeam} v.s. ${gameData.awayteam}`; //codeName
+              const hteam = teamIdMap.get(gameData.hometeam);
+              const ateam = teamIdMap.get(gameData.awayteam);
+              const homeTeamName = teamNameMap.get(gameData.hometeam);
+              const awayTeamName = teamNameMap.get(gameData.awayteam);
+              //const title = `${homeTeamName} v.s. ${awayTeamName}`; //Name
+              const title = `${gameData.hometeam} v.s. ${gameData.awayteam}`; //codeName
 
-                        if (!hteam || !ateam) {
-                            console.error("未找到相应的主队或客队");
-                            continue;  // 如果没有找到队伍，就跳过这场比赛
-                        }
+              if (!userTeam.includes(gameData.hometeam) || !userTeam.includes(gameData.awayteam)) {
+                continue; // Skip to the next game if neither team matches
+              }
 
-                        if (userTeam.includes(gameData.hometeam) && userTeam.includes(gameData.awayteam)) {
-                          continue;
-                        }
 
-                        if (!addedTimestamps.has(timestamp)) {
-                            gamesData.push({
-                                id: timestamp,
-                                title: title,
-                                codeName: teamData.codeName,
-                                start: moment(game.toDate()).format('YYYY-MM-DD HH:mm:ss'),
-                                end: moment(game.toDate()).format('YYYY-MM-DD HH:mm:ss'),
-                                timestamp: timestamp,
-                                teamId: teamId,
-                                hometeamId: hteam,
-                                awayteamId: ateam,
-                                hcodeName: gameData.hometeam,
-                                acodeName: gameData.awayteam,
-                                selectedTeam: gameData.awayteam
-                            });
-                            addedTimestamps.add(timestamp);
-                        }
-                    }
-                }
+              if (!hteam || !ateam) {
+                console.error("未找到相应的主队或客队");
+                continue;  // 如果没有找到队伍，就跳过这场比赛
+              }
+
+             
+              if (!addedTimestamps.has(timestamp)) {
+                gamesData.push({
+                  id: timestamp,
+                  title: title,
+                  codeName: teamData.codeName,
+                  start: moment(game.toDate()).format('YYYY-MM-DD HH:mm:ss'),
+                  end: moment(game.toDate()).format('YYYY-MM-DD HH:mm:ss'),
+                  timestamp: timestamp,
+                  teamId: teamId,
+                  hometeamId: hteam,
+                  awayteamId: ateam,
+                  hcodeName: gameData.hometeam,
+                  acodeName: gameData.awayteam,
+                  selectedTeam: gameData.awayteam
+                });
+                addedTimestamps.add(timestamp);
+              }
             }
+          }
         }
+      }
 
-        setGames(gamesData);
+      setGames(gamesData);
     } catch (error) {
-        console.error("Error fetching games:", error);
+      console.error("Error fetching games:", error);
     }
-};
+  };
 
-useEffect(() => {
+  useEffect(() => {
     fetchGames();
-}, []);
+  }, []);
 
 
-  
-  
-  
-  
+
+
+
+
   // 其餘函數保持不變...
 
-  
-  
+
+
   console.log(games)
   // Handle click on a game event
   const handleGameClick = (game) => {
@@ -163,43 +166,43 @@ useEffect(() => {
   const playerattack = (action) => {
     console.log(`Action "${action}" selected for game:`, selectedGame);
     setDialogOpen(false); // Close the dialog after action
-        
+
     // 获取选定游戏的团队数据
     const selectedGameData = games.find(game => game.timestamp === selectedGame.timestamp);
-    
+
     // 从团队数据中提取codeName 和 hometeam
     const hcodeName = selectedGameData ? selectedGameData.hcodeName : null;
     const hometeamId = selectedGameData ? selectedGameData.hometeamId : null;
 
-    
+
     // 获取选定游戏的团队ID
     const teamId = selectedGameData ? selectedGameData.teamId : null;
-    
+
     // 导航到新页面，同时将codeName、hometeam和teamId添加到查询参数中
     router.push({
       pathname: "/playershow",
-      query: { 
+      query: {
         timestamp: selectedGame.timestamp,
         codeName: hcodeName, // Add codeName to the query
         teamId: hometeamId // Add teamId to the query
       }
     });
   };
-  
+
   const playerdefence = (action) => {
     console.log(`Action "${action}" selected for game:`, selectedGame);
     setDialogOpen(false); // Close the dialog after action
-        
+
     // 获取选定游戏的团队数据
     const selectedGameData = games.find(game => game.timestamp === selectedGame.timestamp);
     const hcodeName = selectedGameData ? selectedGameData.hcodeName : null;
     const hometeamId = selectedGameData ? selectedGameData.hometeamId : null;
 
-    
+
     // 导航到新页面，同时将codeName和teamId添加到查询参数中
     router.push({
       pathname: "/playershow",
-      query: { 
+      query: {
         timestamp: selectedGame.timestamp,
         codeName: hcodeName, // Add codeName to the query
         teamId: hometeamId // Add teamId to the query
@@ -209,20 +212,20 @@ useEffect(() => {
   const awayplayerattack = (action) => {
     console.log(`Action "${action}" selected for game:`, selectedGame);
     setDialogOpen(false); // Close the dialog after action
-        
+
     // 获取选定游戏的团队数据
     const selectedGameData = games.find(game => game.timestamp === selectedGame.timestamp);
-    
+
     // 从团队数据中提取codeName
     const codeName = selectedGameData ? selectedGameData.codeName : null;
-    
+
     // 获取选定游戏的团队ID
     const teamId = selectedGameData ? selectedGameData.teamId : null;
-    
+
     // 导航到新页面，同时将codeName和teamId添加到查询参数中
     router.push({
       pathname: "/playershow",
-      query: { 
+      query: {
         timestamp: selectedGame.timestamp,
         codeName: codeName, // Add codeName to the query
         teamId: teamId // Add teamId to the query
@@ -232,63 +235,63 @@ useEffect(() => {
   const awayplayerdefence = (action) => {
     console.log(`Action "${action}" selected for game:`, selectedGame);
     setDialogOpen(false); // Close the dialog after action
-        
+
     // 获取选定游戏的团队数据
     const selectedGameData = games.find(game => game.timestamp === selectedGame.timestamp);
-    
+
     // 从团队数据中提取codeName
     const codeName = selectedGameData ? selectedGameData.codeName : null;
-    
+
     // 获取选定游戏的团队ID
     const teamId = selectedGameData ? selectedGameData.teamId : null;
-    
+
     // 导航到新页面，同时将codeName和teamId添加到查询参数中
     router.push({
       pathname: "/playershow",
-      query: { 
+      query: {
         timestamp: selectedGame.timestamp,
         codeName: codeName, // Add codeName to the query
         teamId: teamId // Add teamId to the query
       }
     });
   };
-  
-  
+
+
   const recordattack = (action) => {
     console.log(`Action "${action}" selected for game:`, selectedGame);
     setDialogOpen(false); // Close the dialog after action
-    
+
     // 获取选定游戏的团队数据
     const selectedGameData = games.find(game => game.timestamp === selectedGame.timestamp);
-      
+
     // 从团队数据中提取codeName
     const codeName = selectedGameData ? selectedGameData.codeName : null;
 
     const teamId = selectedGameData ? selectedGameData.teamId : null;
 
-    
+
     // 导航到新页面，同时将codeName和teamId添加到查询参数中
     router.push({
       pathname: "/test",
-      query: { 
+      query: {
         timestamp: selectedGame.timestamp,
         codeName: codeName, // Add codeName to the query
         teamId: teamId // Add teamId to the query
       }
     });
   };
-  
 
 
-  
+
+
 
   const handleEditGame = (action) => {
     console.log(`Action "${action}" selected for game:`, selectedGame);
     setDialogOpen(false); // Close the dialog after action
-    
+
     // 获取选定游戏的团队数据
     const selectedGameData = games.find(game => game.timestamp === selectedGame.timestamp);
-      
+
     // 从团队数据中提取codeName
     const codeName = selectedGameData ? selectedGameData.codeName : null;
 
@@ -297,11 +300,11 @@ useEffect(() => {
     setDialogOpen(false); // Close the dialog after action
     router.push({
       pathname: "/edit-game",
-      query: { 
+      query: {
         timestamp: selectedGame.timestamp,
         codeName: codeName, // Add codeName to the query
       }
-    }); 
+    });
   };
 
   return (
