ff49cef795b12c44b06492215bc5f658c00ae883
tt0504 <132889370+tt0504@users.noreply.github.com>
2024-05-30 15:34:50 +0800

    團隊

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,15 +4,34 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
-import { AuthConsumer, AuthProvider } from 'src/contexts/auth-context';
+import { AuthProvider, useAuthContext } from 'src/contexts/auth-context';
 import { useNProgress } from 'src/hooks/use-nprogress';
 import { createTheme } from 'src/theme';
 import { createEmotionCache } from 'src/utils/create-emotion-cache';
 import 'simplebar-react/dist/simplebar.min.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const clientSideEmotionCache = createEmotionCache();
 
 const SplashScreen = () => null;
+
+const AuthWrapper = ({ children }) => {
+  const { isLoading, isAuthenticated } = useAuthContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      router.push('/auth/login');
+    }
+  }, [isLoading, isAuthenticated, router]);
+
+  if (isLoading || !isAuthenticated) {
+    return <SplashScreen />;
+  }
+
+  return children;
+};
 
 const App = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
@@ -38,13 +57,9 @@ const App = (props) => {
         <AuthProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            <AuthConsumer>
-              {
-                (auth) => auth.isLoading
-                  ? <SplashScreen />
-                  : getLayout(<Component {...pageProps} />)
-              }
-            </AuthConsumer>
+            <AuthWrapper>
+              {getLayout(<Component {...pageProps} />)}
+            </AuthWrapper>
           </ThemeProvider>
         </AuthProvider>
       </LocalizationProvider>
